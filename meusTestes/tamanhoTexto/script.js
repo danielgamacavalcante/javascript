a3ec17a1bee80ff81234ad92677d5b8461012a8b
danielgamacavalcante <daniel.gama.cavalcante@gmail.com>
2023-01-12 17:51:20 -0300

    tamanhodetexto

---
+++ meusTestes/tamanhoTexto/script.js
@@ -0,0 +1,14 @@
+function funcprincipal(id){
+
+    
+    //var h1 = document.querySelector(`#${id}`).innerHTML
+
+    //var print = document.querySelector('#texto')
+
+    //print.innerHTML = `Tamanho do Texto: ${h1.length}`
+    console.log(`Tamanho do Texto: ${id.length}`)
+
+}
+funcprincipal('Olá mundo')
+
+
